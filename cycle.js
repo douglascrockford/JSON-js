f89de6216e6f4ce5101b304b6a5110fd27f6dc1b
Douglas Crockford <douglas@crockford.com>
2012-07-19 08:31:29 -0700

    Public Domain

--- cycle.js
+++ cycle.js
@@ -6,15 +6,11 @@
 
     NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
 
-    See http://www.JSON.org/js.html
-
-
     This code should be minified before deployment.
     See http://javascript.crockford.com/jsmin.html
 
     USE YOUR OWN COPY. IT IS EXTREMELY UNWISE TO LOAD CODE FROM SERVERS YOU DO
     NOT CONTROL.
-
 */
 
 /*jslint evil: true, regexp: true */
