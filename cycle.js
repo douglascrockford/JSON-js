40f3377a631eaedeec877379f9cb338046cac0e0
Douglas Crockford <douglas@crockford.com>
2012-08-19 10:25:51 -0700

    For Kyle

--- cycle.js
+++ cycle.js
@@ -1,6 +1,6 @@
 /*
     cycle.js
-    2012-07-18
+    2012-08-19
 
     Public Domain.
 
@@ -54,9 +54,15 @@ if (typeof JSON.decycle !== 'function') {
             case 'object':
 
 // typeof null === 'object', so get out if this value is not really an object.
+// Also get out if it is a weird builtin object.
 
-                if (!value) {
-                    return null;
+                if (value === null ||
+                        value instanceof Boolean ||
+                        value instanceof Date    ||
+                        value instanceof Number  ||
+                        value instanceof RegExp  ||
+                        value instanceof String) {
+                    return value;
                 }
 
 // If the value is an object or array, look to see if we have already
