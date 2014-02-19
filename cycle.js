f5ba5c8f978c6218d843cc68c966410b9b0607fc
Eccenux <eccenux+git@gmail.com>
2014-02-19 09:27:41 +0100

    Allow decycle to dump DOM nodes as string info.

--- cycle.js
+++ cycle.js
@@ -20,7 +20,51 @@
 */
 
 if (typeof JSON.decycle !== 'function') {
-    JSON.decycle = function decycle(object) {
+(function(){
+
+	/**
+	 * Allows stringifing DOM elements.
+	 *
+	 * This is done in hope to identify the node when dumping.
+	 *
+	 * @param {Element} node DOM Node (works best for DOM Elements).
+	 * @returns {String}
+	 */
+	function stringifyNode(node) {
+		var text = "";
+		switch (node.nodeType) {
+			case node.ELEMENT_NODE:
+				text = node.nodeName.toLowerCase();
+				if (node.id.length) {
+					text += '#' + node.id;
+				}
+				else {
+					if (node.className.length) {
+						text += '.' + node.className.replace(/ /, '.');
+					}
+					if ('textContent' in node) {
+						text += '{textContent:'
+								+ (node.textContent.length < 20 ? node.textContent : node.textContent.substr(0, 20) + '...')
+							+ '}'
+						;
+					}
+				}
+			break;
+			// info on values: http://www.w3.org/TR/DOM-Level-2-Core/core.html#ID-1841493061
+			default:
+				text = node.nodeName;
+				if (node.nodeValue !== null) {
+					text += '{value:'
+								+ (node.nodeValue.length < 20 ? node.nodeValue : node.nodeValue.substr(0, 20) + '...')
+						+ '}'
+					;
+				}
+			break;
+		}
+		return text;
+	}
+
+    JSON.decycle = function decycle(object, stringifyNodes) {
         'use strict';
 
 // Make a deep copy of an object or array, assuring that there is at most
@@ -35,11 +79,17 @@ if (typeof JSON.decycle !== 'function') {
 //      return JSON.stringify(JSON.decycle(a));
 // produces the string '[{"$ref":"$"}]'.
 
+// NOTE! If your object contains DOM Nodes you might want to use `stringifyNodes` option
+// This will dump e.g. `div` with id="some-id" to string: `div#some-id`.
+// You will avoid some problems, but you won't to be able to fully retro-cycle.
+// To dump almost any variable use: `alert(JSON.stringify(JSON.decycle(variable, true)));`
+
 // JSONPath is used to locate the unique object. $ indicates the top level of
 // the object or array. [NUMBER] or [STRING] indicates a child member or
 // property.
 
         var objects = [],   // Keep a reference to each unique object or array
+			stringifyNodes = typeof(stringifyNodes) === 'undefined' ? false : stringifyNodes,
             paths = [];     // Keep the path to each unique object or array
 
         return (function derez(value, path) {
@@ -49,6 +99,12 @@ if (typeof JSON.decycle !== 'function') {
             var i,          // The loop counter
                 name,       // Property name
                 nu;         // The new object or array
+
+// if we have a DOM Element/Node convert it to textual info.
+
+			if (stringifyNodes && typeof value === 'object' && value !== null && 'nodeType' in value) {
+				return stringifyNode(value);
+			}
 
 // typeof null === 'object', so go on if this value is really an object but not
 // one of the weird builtin objects.
@@ -99,6 +155,7 @@ if (typeof JSON.decycle !== 'function') {
             return value;
         }(object, '$'));
     };
+})();
 }
 
 
